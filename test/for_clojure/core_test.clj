97edb70b291f253050eebb060c2d3d8d3c0282f8
hlatki <hannahatkinson@gmail.com>
2015-01-25 11:17:12 -0800

    Ugly solution for #105

--- test/for_clojure/core_test.clj
+++ test/for_clojure/core_test.clj
@@ -2,6 +2,13 @@
   (:require [clojure.test :refer :all]
             [for-clojure.core :refer :all]))
 
+(deftest problem-105
+  (testing "Identify keys and values (#105)"
+    (is (= {} (id-key-vals [])))
+    (is (= {:a [1]} (id-key-vals [:a 1])))
+    (is (= {:a [1], :b [2]} (id-key-vals [:a 1, :b 2])))
+    (is (= {:a [1 2 3], :b [], :c [4]} (id-key-vals [:a 1 2 3 :b :c 4])))))
+
 (deftest problem-93
   (testing "Partially flatten a sequence (#93)"
     (is (= (partially-flatten [["Do"] ["Nothing"]]) [["Do"] ["Nothing"]]))
