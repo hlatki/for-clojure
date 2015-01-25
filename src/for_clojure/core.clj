1f90af83f6d2363a8b09e7021806c19d943bbd52
hlatki <hannahatkinson@gmail.com>
2015-01-25 11:17:12 -0800

    sadface

--- src/for_clojure/core.clj
+++ src/for_clojure/core.clj
@@ -3,4 +3,27 @@
 (defn partially-flatten
   "Partially flatten a sequence (#93)"
   [coll]
-  (map flatten x))
+  (map flatten coll))
+
+(defn id-key-vals
+  "Identify keys and values (#105)"
+  [vect]
+  (loop [acc-map {}
+         rest-vect (rest vect)
+         k (first vect)
+         v []
+         i (first rest-vect)]
+    (cond
+      (nil? k) {}
+      (nil? i) (assoc acc-map k v)
+      (keyword? i) (recur (assoc acc-map k v)
+                          (rest rest-vect)
+                          i
+                          []
+                          (first (rest rest-vect)))
+      (number? i)  (recur acc-map
+                          (rest rest-vect)
+                          k
+                          (conj v i)
+                          (first (rest rest-vect)))
+      :else acc-map)))
--- test/for_clojure/core_test.clj
+++ test/for_clojure/core_test.clj
@@ -2,6 +2,13 @@
   (:require [clojure.test :refer :all]
             [for-clojure.core :refer :all]))
 
+(deftest problem-105
+  (testing "Identify keys and values (#105)"
+    (is (= {} (id-key-vals [])))
+    (is (= {:a [1]} (id-key-vals [:a 1])))
+    (is (= {:a [1], :b [2]} (id-key-vals [:a 1, :b 2])))
+    (is (= {:a [1 2 3], :b [], :c [4]} (id-key-vals [:a 1 2 3 :b :c 4])))))
+
 (deftest problem-93
   (testing "Partially flatten a sequence (#93)"
     (is (= (partially-flatten [["Do"] ["Nothing"]]) [["Do"] ["Nothing"]]))
@@ -9,3 +16,5 @@
    [[:a :b] [:c :d] [:e :f]]))
     (is (= (partially-flatten '((1 2)((3 4)((((5 6)))))))
    '((1 2)(3 4)(5 6))))))
+
+
