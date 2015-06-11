b4e82df7338fd6f8c10c30bf0d996149f7feb872
hlatki <hannahatkinson@gmail.com>
2015-06-10 21:16:34 -0700

    Add code and tests for #108

--- src/for_clojure/core.clj
+++ src/for_clojure/core.clj
@@ -5,3 +5,37 @@
   [coll]
   (filter #(and (sequential? %) (not (sequential? (first %))))
     (tree-seq sequential? seq coll)))
+
+(defn id-key-vals
+  "Identify keys and values (#105)"
+  [vect]
+  (loop [acc-map {}
+         rest-vect (rest vect)
+         k (first vect)
+         v []
+         i (first rest-vect)]
+    (cond
+      (nil? k) {}
+      (nil? i) (assoc acc-map k v)
+      (keyword? i) (recur (assoc acc-map k v)
+                          (rest rest-vect)
+                          i
+                          []
+                          (first (rest rest-vect)))
+      (number? i)  (recur acc-map
+                          (rest rest-vect)
+                          k
+                          (conj v i)
+                          (first (rest rest-vect)))
+      :else acc-map)))
+
+(defn smallest-num-in-all-seqs
+  "Return the smallest number in all (sorted) input sequences (#108)"
+  [& seqs]
+  (let [firsts (map first seqs)
+        min-first (apply min firsts)]
+    (if (apply = firsts)
+      min-first
+      (recur (map #(if (= (first %) min-first)
+                    (rest %)
+                    %) seqs)))))
--- test/for_clojure/core_test.clj
+++ test/for_clojure/core_test.clj
@@ -16,3 +16,12 @@
    [[:a :b] [:c :d] [:e :f]]))
     (is (= (partially-flatten '((1 2)((3 4)((((5 6)))))))
    '((1 2)(3 4)(5 6))))))
+
+(deftest problem-108
+  (testing "Smallest number in all sorted seqs"
+    (= 3 (smallest-num-in-all-seqs [3 4 5]))
+    (= 4 (smallest-num-in-all-seqs [1 2 3 4 5 6 7] [0.5 3/2 4 19]))
+    (= 7 (smallest-num-in-all-seqs (range) (range 0 100 7/6) [2 3 5 7 11 13]))
+    (= 64 (smallest-num-in-all-seqs (map #(* % % %) (range)) ;; perfect cubes
+              (filter #(zero? (bit-and % (dec %))) (range)) ;; powers of 2
+              (iterate inc 20)))))
